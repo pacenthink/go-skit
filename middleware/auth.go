e0e927a29cc1d6aa8bee59a0dae2a1f437f7980d
euforia <euforia>
2022-09-23 12:19:35 -0700

    Fix imports

--- middleware/auth.go
+++ middleware/auth.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pacenthink/bnd-project/go-skit/util"
+	"github.com/pacenthink/go-skit/util"
 )
 
 func Authenticate() gin.HandlerFunc {
